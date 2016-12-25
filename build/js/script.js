38275132b083567f798d83bc30845a6c891471c7
Дмитрий Жур <tehzienergy@gmail.com>
2016-12-25 15:15:48 +0300

    scale fixes

--- build/js/script.js
+++ build/js/script.js
@@ -22,4 +22,4 @@ $(document).ready(function(){
 
 
 
-VK.Widgets.Group("vk_groups",{mode:0,width:"220",height:"100"},2679687);})
+VK.Widgets.Group("vk_groups",{mode:0,width:"220",height:"100"},2679687),$(function(){$(window).bind("resize",function(){resizeMe()}).trigger("resize")});var resizeMe=function(){var i=1920,e=10,n=$(window).width(),r=n/i,t=e*r;$("html").css("font-size",t)};})
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,19 @@
 VK.Widgets.Group("vk_groups", {mode: 0, width: "220", height: "100"}, 2679687);
+
+$(function () {
+    $(window).bind('resize', function () {
+        resizeMe();
+    }).trigger('resize');
+});
+
+var resizeMe = function () {
+    //Standard width, for which the body font size is correct
+    var preferredWidth = 1920;
+    //Base font size for the page
+    var fontsize = 10;
+
+    var displayWidth = $(window).width();
+    var percentage = displayWidth / preferredWidth;
+    var newFontSize = fontsize * percentage;
+    $("html").css("font-size", newFontSize);
+};
